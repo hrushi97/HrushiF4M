cd506f51e4e488304ca5a4aa0fb51780114d422f
hrushi97 <ingolehrushikesh@gmail.com>
2023-12-03 20:55:04 +0530

    module

---
+++ Public/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='left-nav'>
+        <h1>Dictionary App</h1>
+      </div>
+      <div className='right-nav'>
+        <Link to='/'>Home</Link>
+        <Link to='/history'>History</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
